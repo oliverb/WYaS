9e0acfda75ec791bc4625babadeaf1186f7bb56d
Oliver Behm <oliverbehm@gmail.com>
2016-02-07 19:10:12 +0100

    Factored list comparison logic out of eqv

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -62,13 +62,15 @@ eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
 eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
 eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
 eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
-eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) &&
-                                                             (all eqvPair $ zip arg1 arg2)
-     where eqvPair (x1, x2) = case eqv [x1, x2] of
-                                Left err -> False
-                                Right (Bool val) -> val
+eqv [(List arg1), (List arg2)]             = return $ Bool $ compareLists eqv arg1 arg2
 eqv [_, _]                                 = return $ Bool False
 eqv badArgList                             = throwError $ NumArgs 2 badArgList
+
+compareLists :: ([LispVal] -> ThrowsError LispVal) -> [LispVal] -> [LispVal] -> Bool
+compareLists cmp xs ys = (length xs == length ys) && (all cmpPair $ zip xs ys)
+    where cmpPair (x, y) = case cmp [x, y] of
+                            Left err -> False
+                            Right (Bool val) -> val
 
 car :: [LispVal] -> ThrowsError LispVal
 car [List (x : xs)]         = return x
