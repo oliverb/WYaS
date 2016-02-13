53b7f677828cfb10f3684c6678b67cc8b7ef6a1b
Oliver Behm <oliverbehm@gmail.com>
2016-02-14 00:22:16 +0100

    Added REPL functionality from tutorial

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Monad.Except
 import System.Environment
+import System.IO
 
 import Types
 import Evaluation
@@ -9,5 +10,29 @@ import Parser
 
 main :: IO ()
 main = do args <- getArgs
-          evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-          putStrLn $ extractValue $ trapError evaled
+          case length args of
+               0 -> runRepl
+               1 -> evalAndPrint $ args !! 0
+               otherwise -> putStrLn "program takes only 0 or 1 argument"
+
+runRepl :: IO ()
+runRepl = until_ (=="quit") (readPrompt "wyas>>> ") evalAndPrint
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError $ (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+    result <- prompt
+    if pred result
+        then return ()
+        else action result >> until_ pred prompt action
